e8fe11da083f6347c7bbdcd80a2241561ef0b6f1
Joshua <joshua.jack@HSEMAs-MacBook-Pro.local>
2018-04-24 09:10:19 -0400

    created

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { PopoverController } from 'ionic-angular';
+import {ListPage} from '../list/list';
+
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  constructor(public popoverCtrl: PopoverController) {
 
   }
+
+  presentPopover(myEvent) {
+    let popover = this.popoverCtrl.create(ListPage);
+    popover.present({
+      ev: myEvent
+    });
+  }
+
+
 }
